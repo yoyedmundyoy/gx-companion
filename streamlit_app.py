c0458456a0fd7bea91ca73b36facb30f4e11a83f
Edmund <50744370+yoyedmundyoy@users.noreply.github.com>
2024-12-28 06:13:13 +0000

    removed chat history

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,26 @@ from snowflake.core import Root
 
 import json
 
+### Prompt
+PROMPT = """
+An excerpt from a document is given below.
+
+---------------------
+{prompt_context}
+---------------------
+
+Given the document excerpt, answer the following query.
+If the context does not provide enough information, decline to answer.
+Do not output anything that can't be answered from the context.
+
+Question: {myquestion}
+Answer:
+"""
+
 
 ### Default Values
 NUM_CHUNKS = 10 # Num-chunks provided as context. Play with this to check how it affects your accuracy
-slide_window = 7
-cmd = """
+CMD = """
             select snowflake.cortex.complete(?, ?) as response
           """
 
@@ -18,7 +33,7 @@ CORTEX_SEARCH_SCHEMA = "DATA"
 CORTEX_SEARCH_SERVICE = "GX_SEARCH_SERVICE"
 
 #connection params
-connection_params = {
+CONNECTION_PARAMS = {
     "account": st.secrets["snowflake"]["account"],
     "user": st.secrets["snowflake"]["user"],
     "password": st.secrets["snowflake"]["password"],
@@ -34,30 +49,24 @@ COLUMNS = [
     # "category"
 ]
 
-session = Session.builder.configs(connection_params).create()
+session = Session.builder.configs(CONNECTION_PARAMS).create()
 
 root = Root(session)
 
 svc = root.databases[CORTEX_SEARCH_DATABASE].schemas[CORTEX_SEARCH_SCHEMA].cortex_search_services[CORTEX_SEARCH_SERVICE]
 
 ### Functions
+
+def init_titles():
+    st.title(f":speech_balloon: Hi, I am your GX Companion")
+    st.write("AI can make mistakes, please check carefully.")
+    st.write("Note: This app is a personal project and has no affliation with GX Bank Berhad.")
      
 def config_options():
 
     st.sidebar.selectbox('Select your model:',(
                                     'mistral-large2'), key="model_name")
 
-    # categories = session.table('docs_chunks_table').select('category').distinct().collect()
-
-    # cat_list = ['ALL']
-    # for cat in categories:
-    #     cat_list.append(cat.CATEGORY)
-            
-    # st.sidebar.selectbox('Select what products you are looking for', cat_list, key = "category_value")
-
-    st.sidebar.checkbox('Do you want that I remember the chat history?', key="use_chat_history", value = True)
-
-    st.sidebar.checkbox('Debug: Click to see summary generated of previous conversation', key="debug", value = True)
     st.sidebar.button("Start Over", key="clear_conversation", on_click=init_messages)
     st.sidebar.expander("Session State").write(st.session_state)
 
@@ -66,159 +75,52 @@ def init_messages():
     # Initialize chat history
     if st.session_state.clear_conversation or "messages" not in st.session_state:
         st.session_state.messages = []
+    
+    if not st.session_state.messages:
+        st.session_state.messages.append({"role": "assistant", "content": "Ask me anything about GX Bank's products and campaigns!"})
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 def get_similar_chunks_search_service(query):
 
-    # if st.session_state.category_value == "ALL":
-    #     response = svc.search(query, COLUMNS, limit=NUM_CHUNKS)
-    # else: 
-    #     filter_obj = {"@eq": {"category": st.session_state.category_value} }
-    #     response = svc.search(query, COLUMNS, filter=filter_obj, limit=NUM_CHUNKS)
-
     response = svc.search(query, COLUMNS, limit=NUM_CHUNKS)
-    st.sidebar.json(response.json())
+    # st.sidebar.json(response.json())
     
     return response.json()  
 
-def get_chat_history():
-#Get the history from the st.session_stage.messages according to the slide window parameter
-    
-    chat_history = []
-    
-    start_index = max(0, len(st.session_state.messages) - slide_window)
-    for i in range (start_index , len(st.session_state.messages) -1):
-         chat_history.append(st.session_state.messages[i])
-
-    return chat_history
-
-def summarize_question_with_history(chat_history, question):
-# To get the right context, use the LLM to first summarize the previous conversation
-# This will be used to get embeddings and find similar chunks in the docs for context
-
-    prompt = f"""
-        You are a summarizer. 
-        Based on the question and the chat history, figure out what the user is trying to ask.
-
-        <chat_history>
-        {chat_history}
-        </chat_history>
-        <question>
-        {question}
-        </question>
-
-        <final_format>
-        Original Question: Put the original question here without changing anything.
-
-        Summarized Question: Using the chat history, create a comprehensive question. Do not provide any explanation or start with "considering", only provide the final question.
-        </final_format>
-        """
-
-    df_response = session.sql(cmd, params=[st.session_state.model_name, prompt]).collect()
-    res_text = df_response[0].RESPONSE
-    
-
-
-    if st.session_state.debug:
-        st.sidebar.text("Summary to be used to find similar chunks in the docs:")
-        st.sidebar.caption(res_text)
-
-    res_text = res_text.replace("'", "")
-
-    return res_text
-
 def create_prompt (myquestion):
 
-    if st.session_state.use_chat_history:
-        chat_history = get_chat_history()
-
-        if chat_history != []: #There is chat_history, so not first question
-            question_summary = summarize_question_with_history(chat_history, myquestion)
-            prompt_context =  get_similar_chunks_search_service(question_summary)
-        else:
-            prompt_context = get_similar_chunks_search_service(myquestion) #First question when using history
-    else:
-        prompt_context = get_similar_chunks_search_service(myquestion)
-        chat_history = ""
-  
-# Dobby is a free assistant who chooses to help because of his enormous heart.",
-#         "Extremely devoted and will go to any length to help his friends.",
-#         "Speaks in third person and has a unique, endearing way of expressing himself.",
-#         "Known for his creative problem-solving, even if his solutions are sometimes unconventional
-
-    prompt = f"""
-            <system_prompt>
-            You are GX Companion, an expert customer service chat assistance of GX Bank.
-            You are patient, helpful, kind, and courteous.
-            You treat customers with respect and humility, you are never rude and never swear.
-            Answer user queries in the context of GX bank and always answer in first person.
-            You can extract information from the CONTEXT provided
-            between <context> and </context> tags.
-            You offer a chat experience considering the information included in the CHAT HISTORY
-            provided between <chat_history> and </chat_history> tags..
-            When ansering the question contained between <question> and </question> tags
-            be concise and comprehensive to attend to the question and do not hallucinate. 
-            Do not return answers that are too long, you can use bullet points where necessary.
-            If you don´t have the information just say so.
-            Include information from the context provided where you think might be useful.
-            
-            Do not mention the CONTEXT used in your answer.
-            Do not mention the CHAT HISTORY used in your asnwer.
-
-            Only anwer the question if you can extract it from the CONTEXT provideed.
-            </system_prompt>
-            
-            <chat_history>
-            {chat_history}
-            </chat_history>
-            <context>          
-            {prompt_context}
-            </context>
-            <question>  
-            {myquestion}
-            </question>
-            Answer: 
-            """
+    prompt_context = get_similar_chunks_search_service(myquestion)
     
     json_data = json.loads(prompt_context)
 
     relative_paths = set(item['relative_path'] for item in json_data['results'])
 
-    return prompt, relative_paths
-
+    return PROMPT.format(prompt_context=prompt_context, myquestion=myquestion), relative_paths
 
 def answer_question(myquestion):
 
     prompt, relative_paths =create_prompt (myquestion)
-    df_response = session.sql(cmd, params=[st.session_state.model_name, prompt]).collect()
+    df_response = session.sql(CMD, params=[st.session_state.model_name, prompt]).collect()
     return df_response, relative_paths
 
 def main():
-    
-    st.title(f":speech_balloon: Hi, I am your GX Companion")
-    st.write("Ask me anything about GX Bank's products and campaigns.")
-    st.write("AI can make mistakes, please check carefully.")
-    st.write("Note: This app is a personal project and has no affliation with GX Bank Berhad.")
-    # docs_available = session.sql("ls @docs").collect()
-    # list_docs = []
-    # for doc in docs_available:
-    #     list_docs.append(doc["name"])
-    # st.dataframe(list_docs)
-
+    """Main function to run the application logic."""
+    init_titles()
     config_options()
     init_messages()
-     
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-    
+
     # Accept user input
-    if question := st.chat_input("What do you want to know about your products?"):
+    if question := st.chat_input("Message GX Companion"):
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": question})
+        
         # Display user message in chat message container
         with st.chat_message("user"):
             st.markdown(question)
+
         # Display assistant response in chat message container
         with st.chat_message("assistant"):
             message_placeholder = st.empty()
@@ -230,17 +132,6 @@ def main():
                 res_text = response[0].RESPONSE
                 res_text = res_text.replace("'", "")
                 message_placeholder.markdown(res_text)
-
-                if relative_paths != "None":
-                    with st.sidebar.expander("Related Documents"):
-                        for path in relative_paths:
-                            cmd2 = f"select GET_PRESIGNED_URL(@docs, '{path}', 360) as URL_LINK from directory(@docs)"
-                            df_url_link = session.sql(cmd2).to_pandas()
-                            url_link = df_url_link._get_value(0,'URL_LINK')
-                
-                            display_url = f"Doc: [{path}]({url_link})"
-                            st.sidebar.markdown(display_url)
-
         
         st.session_state.messages.append({"role": "assistant", "content": res_text})
     
