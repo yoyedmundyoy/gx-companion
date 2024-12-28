9ce684130892d68f01a9b20b2a3c2f796d72396f
Edmund <50744370+yoyedmundyoy@users.noreply.github.com>
2024-12-28 08:01:35 +0000

    refactored and add RAG class for trulens

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 from snowflake.snowpark import Session
 from snowflake.core import Root
 
-import json
-
 ### Prompt
 PROMPT = """
 An excerpt from a document is given below.
@@ -20,17 +18,9 @@ Question: {myquestion}
 Answer:
 """
 
-
 ### Default Values
 NUM_CHUNKS = 10 # Num-chunks provided as context. Play with this to check how it affects your accuracy
-CMD = """
-            select snowflake.cortex.complete(?, ?) as response
-          """
-
-# service params
-CORTEX_SEARCH_DATABASE = "GX_COMPANION"
-CORTEX_SEARCH_SCHEMA = "DATA"
-CORTEX_SEARCH_SERVICE = "GX_SEARCH_SERVICE"
+CMD = "select snowflake.cortex.complete(?, ?) as response"
 
 #connection params
 CONNECTION_PARAMS = {
@@ -42,6 +32,11 @@ CONNECTION_PARAMS = {
     "schema": st.secrets["snowflake"]["schema"]
 }
 
+# service params
+CORTEX_SEARCH_DATABASE = "GX_COMPANION"
+CORTEX_SEARCH_SCHEMA = "DATA"
+CORTEX_SEARCH_SERVICE = "GX_SEARCH_SERVICE"
+
 # columns to query in the service
 COLUMNS = [
     "chunk",
@@ -50,19 +45,33 @@ COLUMNS = [
 ]
 
 session = Session.builder.configs(CONNECTION_PARAMS).create()
-
 root = Root(session)
-
 svc = root.databases[CORTEX_SEARCH_DATABASE].schemas[CORTEX_SEARCH_SCHEMA].cortex_search_services[CORTEX_SEARCH_SERVICE]
+
+class RAG:
+    def retrieve(self, query: str) -> list:
+        context_retrieved = svc.search(query, COLUMNS, limit=NUM_CHUNKS)
+        return context_retrieved
+
+    def complete(self, query: str, context_str: list) -> str:
+        prompt = PROMPT.format(prompt_context=context_str, myquestion=query)
+        df_response = session.sql(CMD, params=[st.session_state.model_name, prompt]).collect()
+        
+        return df_response[0].RESPONSE
+
+    def query(self, query: str) -> str:
+        context_str = self.retrieve(query=query)
+        completion = self.complete(
+            query=query, context_str=context_str
+        )
+        return completion
 
 ### Functions
 
-def init_titles():
+def init_app():
     st.title(f":speech_balloon: Hi, I am your GX Companion")
     st.write("AI can make mistakes, please check carefully.")
     st.write("Note: This app is a personal project and has no affliation with GX Bank Berhad.")
-     
-def config_options():
 
     st.sidebar.selectbox('Select your model:',(
                                     'mistral-large2'), key="model_name")
@@ -71,7 +80,6 @@ def config_options():
     st.sidebar.expander("Session State").write(st.session_state)
 
 def init_messages():
-
     # Initialize chat history
     if st.session_state.clear_conversation or "messages" not in st.session_state:
         st.session_state.messages = []
@@ -83,33 +91,10 @@ def init_messages():
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-def get_similar_chunks_search_service(query):
-
-    response = svc.search(query, COLUMNS, limit=NUM_CHUNKS)
-    # st.sidebar.json(response.json())
-    
-    return response.json()  
-
-def create_prompt (myquestion):
-
-    prompt_context = get_similar_chunks_search_service(myquestion)
-    
-    json_data = json.loads(prompt_context)
-
-    relative_paths = set(item['relative_path'] for item in json_data['results'])
-
-    return PROMPT.format(prompt_context=prompt_context, myquestion=myquestion), relative_paths
-
-def answer_question(myquestion):
-
-    prompt, relative_paths =create_prompt (myquestion)
-    df_response = session.sql(CMD, params=[st.session_state.model_name, prompt]).collect()
-    return df_response, relative_paths
-
 def main():
     """Main function to run the application logic."""
-    init_titles()
-    config_options()
+    rag = RAG()
+    init_app()
     init_messages()
 
     # Accept user input
@@ -127,9 +112,8 @@ def main():
     
             question = question.replace("'","")
     
-            with st.spinner(f"{st.session_state.model_name} thinking..."):
-                response, relative_paths = answer_question(question)            
-                res_text = response[0].RESPONSE
+            with st.spinner(f"{st.session_state.model_name} thinking..."):   
+                res_text = rag.query(question);
                 res_text = res_text.replace("'", "")
                 message_placeholder.markdown(res_text)
         
